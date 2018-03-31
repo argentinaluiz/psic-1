8ab7f09f33a144dcfe936ead3578912558387ed3
Marisa Endruveit <mendruveit@gmail.com>
2018-03-31 19:45:07 -0300

    Mixins para organizar os componentes

--- resources/assets/spa/js/components/psych/PsychMenu.vue
+++ resources/assets/spa/js/components/psych/PsychMenu.vue
@@ -1,30 +1,17 @@
 <template src="../templates/menu.html"></template>
 
 <script type="text/javascript">
-    import store from '../../store/store';
-    import LogoutComponent from '../Logout.vue';
+    import authMixin from '../../mixins/auth.mixin';
+    import menuMixin from '../../mixins/menu.mixin';
 
     export default {
-        components: {
-            'logout': LogoutComponent,
-        },
+        mixins: [menuMixin],
         data(){
             return {
                 menus:[
                     {name: "Minhas classes", routeName: 'class_informations.list' }
                 ],
                 brandRouteName: 'class_informations.list'
-            }
-        },
-        computed: {
-            isAuth(){//Verifica se o usuário está com token válido, ou seja, autenticado
-                return store.state.auth.check;
-            },
-            user(){//Retorna o objeto de dados do usuário descriptografado do token
-                return store.state.auth.user;
-            },
-            username(){//Retorna apenas o nome do usuário, caso o usuário esteja autenticado corretamente
-                return this.isAuth?store.state.auth.user.name:null;
             }
         }
     };
