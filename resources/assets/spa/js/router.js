9f68ff189125fe48f4006621cfba192318f70150
Marisa Endruveit <mendruveit@gmail.com>
2018-03-31 19:09:06 -0300

    Proteção da área interna do SPA

--- resources/assets/spa/js/router.js
+++ resources/assets/spa/js/router.js
@@ -2,9 +2,17 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routes from './router.map';
 import AppComponent from './components/App.vue';
+import store from './store/store';
 
 const router = new VueRouter({
     routes
+});
+//Quando trabalhamos com vue-router, temos um recurso que trabalha como um interceptador nas rotas chamado beforeEach. Isso quer dizer que vamos conferir o token antes que cada página seja renderizada. Também devemos verificar se a página é realmente protegida, pois pode se tratar de uma página liberada para qualquer usuário, mesmo sem autenticação.
+router.beforeEach((to, from, next) => {
+    if (to.meta.auth && !store.state.auth.check) {
+        return router.push({name: 'login'});
+    }
+    next();
 });
 
 new Vue({
--- resources/assets/spa/js/router.map.js
+++ resources/assets/spa/js/router.map.js
@@ -1,8 +1,14 @@
+/*Adicionamos a propriedade meta. Esta propriedade é um objeto que pode conter várias informações, porém só adicionamos uma propriedade auth com valor true.
+A rota login não precisa ser protegida, então não adicionamos. À partir de agora devemos adicionar a propriedade meta em todas as rotas protegidas.*/
+
 export default [
      {
         name: 'class_informations.list',
         path: '/classes',
         component: require('./components/psych/PsychClassInformationList.vue'),
+        meta: {
+            auth: true
+        }
     },
     {
         name: 'login',
