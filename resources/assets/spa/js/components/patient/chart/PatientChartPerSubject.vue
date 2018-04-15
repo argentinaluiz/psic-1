884cdf3de739b9b33282c2a6d459b1a11d90d8b3
Marisa Endruveit <mendruveit@gmail.com>
2018-04-15 10:14:33 -0300

    Testes

--- resources/assets/spa/js/components/patient/chart/PatientChartPerSubject.vue
+++ resources/assets/spa/js/components/patient/chart/PatientChartPerSubject.vue
@@ -12,7 +12,7 @@
                 <div class="col-sm-12">
                     <div class="ibox float-e-margins">
                         <div class="ibox-title">
-                            <h5>(Classe)????
+                            <h5>Definir Nome????
                             <small>{{classInformationName}}</small>
                             </h5>
                             <div class="ibox-tools">
